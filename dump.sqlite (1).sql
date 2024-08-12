21a8f5e03dda0cf4d90c7dd18635958804b91844
aleeremo <159054970+aleeremo@users.noreply.github.com>
2024-08-12 13:54:18 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,22 @@
+-- TABLE
+CREATE TABLE CAMPO(
+  ID INT (4),
+  CIDADE VARCHAR (30) UNIQUE,
+  CAPACIDADE INT (4) NOT NULL
+  
+  
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogador(
+  ID INT(4),
+  NOMNE VARCHAR (30),
+  ESPORTE VARCHAR (30)
+  
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
